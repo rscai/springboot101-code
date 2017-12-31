8fe428a8b0d5af7fd4015772a29128fb367fb825
Ray Cai <ray.s.cai@icloud.com>
2017-12-31 21:41:58 +0800

    web test code

---
+++ web/src/test/java/io/github/rscai/springboot101/web/CommentTest.java
@@ -0,0 +1,54 @@
+package io.github.rscai.springboot101.web;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.rscai.springboot101.web.model.Comment;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentTest {
+
+  private static final String ENDPOINT = "/api/post/{postId}/comment";
+
+  @Autowired
+  protected WebApplicationContext context;
+
+  private MockMvc mvc;
+  private ObjectMapper mapper;
+
+  @Before
+  public void setUp() throws Exception {
+    mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    mapper = new ObjectMapper();
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.configure(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES, false);
+  }
+
+  @Test
+  public void testCreate() throws Exception {
+    final long postId = 1234;
+    final Comment comment = new Comment();
+    comment.setContent("comment content");
+
+    mvc.perform(post(ENDPOINT, postId).accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(comment)))
+        .andExpect(status().isCreated()).andExpect(jsonPath("$.content", is("comment content")))
+        .andExpect(jsonPath("$.createdAt", notNullValue()));
+  }
+}
---
+++ web/src/test/java/io/github/rscai/springboot101/web/PostTest.java
@@ -0,0 +1,70 @@
+package io.github.rscai.springboot101.web;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.rscai.springboot101.web.model.Post;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostTest {
+
+  private static final String ENDPOINT = "/api/post";
+
+  @Autowired
+  protected WebApplicationContext context;
+
+  private MockMvc mvc;
+  private ObjectMapper mapper;
+
+  @Before
+  public void setUp() throws Exception {
+    mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    mapper = new ObjectMapper();
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.configure(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES, false);
+  }
+
+  @Test
+  public void testCreate() throws Exception {
+    final Post input = new Post();
+    input.setContent("post content");
+
+    mvc.perform(
+        post(ENDPOINT).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(input))).andExpect(status().isCreated())
+        .andExpect(jsonPath("$.content", is("post content")))
+        .andExpect(jsonPath("$.createdAt", notNullValue()));
+  }
+
+  @Test
+  public void testUpdate() throws Exception {
+    final long postId = 1234;
+
+    final Post post = new Post();
+    post.setContent("new content");
+
+    mvc.perform(put(ENDPOINT + "/{id}", postId).accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(mapper.writeValueAsString(post))).andExpect(status().isOk()).andDo(print())
+        .andExpect(jsonPath("$.content", is("new content")));
+  }
+
+}
